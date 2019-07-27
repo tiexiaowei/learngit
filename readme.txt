05ffb84c2a0aa9f5bd052f907888657f6b26ee4e
tiexiaowei <578997096@qq.com>
2019-07-27 15:29:30 +0800

    增加文件

--- readme.txt
+++ readme.txt
@@ -3,3 +3,4 @@ git is free software
 the file is stored in end
 Git has a mutable index called stage
 
+   123
---
+++ zenshidai/com.zxsd.app/Boot.java
@@ -0,0 +1,14 @@
+public class Boot {
+    /**
+     * 年龄
+     */
+    private Integer age;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
